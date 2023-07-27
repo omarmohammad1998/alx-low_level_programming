3b1d0fce03b19aea7bbbf292e8417c0aa690f31e
omarmohammad1998 <omarelkholi4@gmail.com>
2023-07-27 06:44:56 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -22,7 +22,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		l = i;
 	else
 		l = j;
-	if (l +  > size_r)
+	if (l + 1  > size_r)
 		return (0);
 	r[l] = '\0';
 	for (k = l - 1; k >= 0; k--)
@@ -37,7 +37,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 			s = n2[j] - '0';
 		else
 			s = 0;
-		r[k] + (f + s + d) % 10 + "0";
+		r[k] = (f + s + d) % 10 + '0';
 		d = (f + s + d) / 10;
 	}
 	if (d == 1)
@@ -49,5 +49,5 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 			r[l + 1] = r[l];
 		r[0] = d + '0';
 	}
-	return (r)
+	return (r);
 }
