e72567ca1be524582a4b783e2cd611944a183873
omarmohammad1998 <omarelkholi4@gmail.com>
2023-07-31 04:55:06 -0700

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -2,7 +2,7 @@
 /**
  * _memcpy - a function that copies memory area
  * @dest: memory where is stored
- * @2src: memory where is copied
+ * @src: memory where is copied
  * *@n: number of bytes
  *
  * Return: copied memory with n byted changed
