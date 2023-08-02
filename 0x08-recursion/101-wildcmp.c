556ec1332293152049ed7b0f120510fc1ead86f4
omarmohammad1998 <omarelkholi4@gmail.com>
2023-08-02 13:06:46 -0700

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -19,6 +19,10 @@ int wildcmp(char *s1, char *s2)
 	}
 	if (*s2 == '*')
 	{
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	else if (*s1 == *s2)
+	{
 		return (wildcmp(s1 + 1, s2 + 1));
 	}
 	return (0);
