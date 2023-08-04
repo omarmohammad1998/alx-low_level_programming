06dd4e1367e79a56220239ef53c7c2c695b99244
omarmohammad1998 <omarelkholi4@gmail.com>
2023-08-04 12:25:09 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -23,14 +23,12 @@ int _atio(char *s)
 
 	while (i < len && f == 0)
 	{
-		if (s[i] == '_')
+		if (s[i] == '-')
 			++d;
 
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			digit = s[i] - '0';
-			if (d % 2)
-				digit = -digit;
 			if (d % 2)
 				digit = -digit;
 			n = n * 10 + digit;
