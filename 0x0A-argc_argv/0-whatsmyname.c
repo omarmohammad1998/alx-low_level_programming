f8d7c3cda7c156ec78744747d85a076755b95033
omarmohammad1998 <omarelkholi4@gmail.com>
2023-08-04 05:44:05 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - prints the name
+ * @argc: num of ar
+ * @argv: array of args
+ *
+ * Return: Always 0
+ */
+
+int main(int arg __attribute__((unused)), char -argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,6 @@
+#define MAIN_H
+
+int _putchar(char c);
+int _atio(char *s);
+
+#endif
