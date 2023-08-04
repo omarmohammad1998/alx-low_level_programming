7ffebd19685ce74357d29fcb438b026e6374cc00
omarmohammad1998 <omarelkholi4@gmail.com>
2023-08-04 06:03:20 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print the num 
+ * @argc: num of
+ * @argv: array of
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+(void) argv: /*ignore argv*/
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
