d3b2004edc22eb8b8c478e4f095a34cbab47bcb4
omarmohammad1998 <omarelkholi4@gmail.com>
2023-08-10 11:36:56 -0700

    101-mul.c

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -12,5 +12,8 @@ int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _atoi(const char *s);
 void print_int(unsigned long int n);
+int _strlen(char *s);
+void errors(void);
+int is_digit(char *s);
 
 #endif
