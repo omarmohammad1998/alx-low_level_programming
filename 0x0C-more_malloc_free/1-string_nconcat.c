e6daea4b00fa69595774842d023bb420c8e105e6
omarmohammad1998 <omarelkholi4@gmail.com>
2023-08-10 07:47:30 -0700

    1-string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -32,6 +32,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		return (NULL);
 	}
+	/*copy first string int*/
+	for (i = 0; s1[i] != '\0'; i++)
+		str[i] = s1[i];
 	/*copy second string into str.*/
 	for (j = 0; j < n; j++)
 	{
