892ae7432674f6222834552051391f2266f96f8f
omarmohammad1998 <omarelkholi4@gmail.com>
2023-08-14 14:28:55 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -57,31 +57,29 @@ char *_strcpy(char *dest, char *src)
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *dog;
+	dog_t *d;
 	int len1, len2;
 
 	len1 = _strlen(name);
 	len2 = _strlen(owner);
-	dog = malloc(sizeof(dog_t));
-	if (dog == NULL)
+	d = malloc(sizeof(dog_t));
+	if (d == NULL)
 		return (NULL);
-
-	dog->name = malloc(sizeof(char) * (len1 + 1));
-	if (dog->name == NULL)
+	d->name = malloc(sizeof(char) * (len1 + 1));
+	if (d->name == NULL)
 	{
-		free(dog);
+		free(d);
 		return (NULL);
 	}
-	dog->owner = malloc(sizeof(char) * (len2 + 1));
-	if (dog->owner == NULL)
+	d->owner = malloc(sizeof(char) * (len2 + 1));
+	if (d->owner == NULL)
 	{
-		free(dog);
-		free(dog->name);
+		free(d);
+		free(d->name);
 		return (NULL);
 	}
-	_strcpy(dog->name, name);
-	_strcpy(dog->owner, owner);
-	dog->age = age;
-
-	return (dog);
+	_strcpy(d->name, name);
+	_strcpy(d->owner, owner);
+	d->age = age;
+	return (d);
 }
