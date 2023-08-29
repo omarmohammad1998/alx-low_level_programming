7306742acbf060a72c86fa4ed87a649311f258c2
omarmohammad1998 <omarelkholi4@gmail.com>
2023-08-29 09:30:49 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * print_listing - prints all the elements of a linked list
+ * print_listint - prints all the elements of a linked list
  * @h: linked list
  *
  * Return: number of nodes
